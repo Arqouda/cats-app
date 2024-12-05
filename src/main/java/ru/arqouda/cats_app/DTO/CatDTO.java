bc02677457c4b5b839ac20d7d216fa6e938fc143
Medvedev Vladislav <vladmed118@gmail.com>
2024-12-05 06:43:42 +0300

    add DTO and swagger

---
+++ src/main/java/ru/arqouda/cats_app/DTO/CatDTO.java
@@ -0,0 +1,13 @@
+package ru.arqouda.cats_app.DTO;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class CatDTO {
+    String name;
+    int weight;
+    int age;
+}
--- src/main/java/ru/arqouda/cats_app/controller/MainController.java
+++ src/main/java/ru/arqouda/cats_app/controller/MainController.java
@@ -1,6 +1,7 @@
 package ru.arqouda.cats_app.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.arqouda.cats_app.DTO.CatDTO;
 import ru.arqouda.cats_app.model.Cat;
 import ru.arqouda.cats_app.repository.CatRepo;
 
@@ -17,14 +19,26 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@Tag(name = "main_methods")
 public class MainController {
 
     @Autowired
     private CatRepo catRepo;
 
+    @Operation(
+            summary = "кладет нового котика в базу",
+            description = "Получает DTO кота и билдером собирает и сохраняет сущность в базу"
+    )
     @PostMapping("api/add")
-    public void addCat(@RequestBody Cat cat) {
-        log.info("New row: " + catRepo.save(cat));
+    public void addCat(@RequestBody CatDTO catDTO) {
+        log.info(
+                "New row: " + catRepo.save(
+                        Cat.builder()
+                            .age(catDTO.getAge())
+                            .weight(catDTO.getWeight())
+                            .name(catDTO.getName())
+                            .build())
+        );
     }
 
     @GetMapping("api/all")
@@ -36,15 +50,16 @@ public class MainController {
     public Cat getCat(@RequestParam Long id) {
         return catRepo.findById(id).orElseThrow();
     }
+
     @DeleteMapping("api/")
-    public void deleteCat(@RequestParam Long id){
+    public void deleteCat(@RequestParam Long id) {
         catRepo.deleteById(id);
     }
 
     @PutMapping("/api/add")
     public String changeCat(@RequestParam Cat cat) {
-        if(!catRepo.existsById(cat.getId())){
-             return "This cat as exist";
+        if (!catRepo.existsById(cat.getId())) {
+            return "This cat as exist";
         }
         return catRepo.save(cat).toString();
     }
